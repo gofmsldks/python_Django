a63f8ed1005b62da603066e8b183d39540864cd5
rlagudndjs95@naver.com <hyungwon95@>
2021-02-11 03:31:38 +0900

    0211

--- pybo/urls.py
+++ pybo/urls.py
@@ -19,4 +19,19 @@ from pybo import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
+
+'''
+제너릭 뷰 url
+
+from django.urls import path
+
+from . import views
+
+app_name = 'pybo'
+urlpatterns = [
+    path('', views.IndexView.as_view()),
+    path('<int:pk>/', views.DetailView.as_view()),
+]
+'''
--- pybo/views.py
+++ pybo/views.py
@@ -3,7 +3,37 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 def index(requeset):
-    return HttpResponse("안녕하세요")
 
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+
+    return render(requeset, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+
+    return render(request, 'pybo/question_detail.html', context)
+
+'''
+제너릭뷰
+class IndexView(generic.ListView):
+    """
+    pybo 목록 출력
+    """
+    def get_queryset(self):
+        return Question.objects.order_by('-create_date')
+
+
+class DetailView(generic.DetailView):
+    """
+    pybo 내용 출력
+    """
+    model = Question
+    
+'''
